e9a194901a253bb7bffefb03a74545ee2836fb8d
swagata0207 <its.swagata.sasmal@gmail.com>
2025-04-04 01:42:30 +0530

    fixed mapper test cases, java 21 update

--- models/src/test/java/mapper/ObjectConverterTest.java
+++ models/src/test/java/mapper/ObjectConverterTest.java
@@ -4,14 +4,42 @@ import dto.CommonUserDTO;
 import entity.CommonUserEntity;
 import org.instancio.Instancio;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.mockito.Mockito.when;
+import static org.instancio.Select.field;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {ObjectConverterImpl.class})
 class ObjectConverterTest {
+
+    ObjectConverter objectConverter = Mappers.getMapper(ObjectConverter.class);
 
     @Test
     void objectConverterTester() {
-        CommonUserEntity commonUser = Instancio.of(CommonUserEntity.class).create();
-        CommonUserDTO commonUserDTO = Instancio.of(CommonUserDTO.class).create();
+        CommonUserEntity commonUser = Instancio.of(CommonUserEntity.class)
+                .set(field(CommonUserEntity::getUserId),"1000")
+                .set(field(CommonUserEntity::getFirstName), "John")
+                .set(field(CommonUserEntity::getLastName), "Doe")
+                .create();
+
+        CommonUserDTO commonUserDTO = Instancio.of(CommonUserDTO.class)
+                .set(field(CommonUserDTO::getUserId), "1000")
+                .set(field(CommonUserDTO::getFirstName), "John")
+                .set(field(CommonUserDTO::getLastName), "Doe")
+                .create();
+
+        CommonUserDTO exportedUserDTO = objectConverter.exportDto(commonUser);
+        CommonUserEntity exportedUser = objectConverter.exportEntity(commonUserDTO);
+
+        assertEquals(commonUser.getUserId(), exportedUserDTO.getUserId());
+        assertEquals(commonUser.getFirstName(), exportedUserDTO.getFirstName());
+        assertEquals(commonUser.getLastName(), exportedUserDTO.getLastName());
+        assertEquals(commonUserDTO.getUserId(), exportedUser.getUserId());
+        assertEquals(commonUserDTO.getFirstName(), exportedUser.getFirstName());
+        assertEquals(commonUserDTO.getLastName(), exportedUser.getLastName());
     }
 }
