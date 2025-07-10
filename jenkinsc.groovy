e5ecb27c34d48565970b7f9717a4045e1a1319c2
swagata0207 <its.swagata.sasmal@gmail.com>
2025-07-11 02:16:05 +0530

    S-3 added build step

--- jenkinsc.groovy
+++ jenkinsc.groovy
@@ -1,4 +1,9 @@
 node {
+    stage('Build') {
+        steps {
+            sh 'gradle clean build'
+        }
+    }
     stage('SonarCloud analysis') {
         withSonarQubeEnv() { // Will pick the global server connection you have configured
             sh 'gradle sonarqube'
