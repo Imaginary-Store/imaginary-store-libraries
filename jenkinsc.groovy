81abb7948c24ee2a267d73e62a148309ccafd54d
swagata0207 <its.swagata.sasmal@gmail.com>
2025-07-11 02:00:18 +0530

    S-3 jenkinsc update

--- jenkinsc.groovy
+++ jenkinsc.groovy
@@ -1,7 +1,7 @@
 node {
     stage('SonarCloud analysis') {
         withSonarQubeEnv() { // Will pick the global server connection you have configured
-            sh './gradlew sonar'
+            sh 'gradle sonarqube'
         }
     }
 }
