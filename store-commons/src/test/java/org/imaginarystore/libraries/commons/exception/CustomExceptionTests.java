78906905304c96f3f28d2830473cfa6e7b944529
swagata0207 <its.swagata.sasmal@gmail.com>
2026-05-06 23:55:17 +0530

    Modified tests

--- store-commons/src/test/java/org/imaginarystore/libraries/commons/exception/CustomExceptionTests.java
+++ store-commons/src/test/java/org/imaginarystore/libraries/commons/exception/CustomExceptionTests.java
@@ -3,12 +3,19 @@ package org.imaginarystore.libraries.commons.exception;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CustomExceptionTests {
 
     @Test
     void DuplicateInsertionExceptionTest() {
-        DuplicateUserInsertionException exception = new DuplicateUserInsertionException("Duplicate user found.");
+        String message = "Duplicate user found.";
+        DuplicateUserInsertionException exception = assertThrows(
+                DuplicateUserInsertionException.class,
+                () -> {
+                    throw new DuplicateUserInsertionException(message);
+                }
+        );
 
         assertEquals("Duplicate user found.", exception.getMessage());
     }
